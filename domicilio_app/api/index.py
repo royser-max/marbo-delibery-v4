bae7b799eb5ef2a6799adc97dcfda0f19b3932b2
royser-max <dzibroymis.mizz@gmail.com>
2025-07-10 16:30:17 -0600

    Delete domicilio_app directory

--- domicilio_app/api/index.py
+++
@@ -1,5 +0,0 @@
-from vercel_wsgi import handle
-from backend.app import app
-
-def handler(environ, start_response):
-    return handle(app, environ, start_response)
--- domicilio_app/backend/auth.py
+++
@@ -1,9 +0,0 @@
-
-from flask_login import LoginManager, UserMixin
-from models import User
-
-login_manager = LoginManager()
-
-@login_manager.user_loader
-def load_user(user_id):
-    return User.query.get(int(user_id))
